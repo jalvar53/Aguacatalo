0867e32a5a232f74c040fb710b91c6832f4a05ae
PC\Aleochoam <alejo8a_2@>
2019-03-26 20:21:14 -0500

    Clean code in youtube

--- commands/music/youtube_music.js
+++ commands/music/youtube_music.js
@@ -1,4 +1,4 @@
-const YTSearch = require('youtube-api-search');
+const { YouTube } = require('better-youtube-api');
 const commando = require('discord.js-commando');
 const ytdl = require('ytdl-core');
 
@@ -10,32 +10,33 @@ class YouTubePlayer extends commando.Command {
       memberName: 'add',
       description: 'reproduce tu tema de youtube',
     });
+
     this.queue = [];
+    this.youtube = new YouTube(process.env.GOOGLE_KEY);
+    this.long_url_pattern = new RegExp('(https?://)*(www.)?youtube.com/(.*)');
+    this.short_url_patter = new RegExp('(https*://)*(www.){0,1}youtu.be/(.*)');
   }
 
   async run(message, args) {
     const streamOptions = { seek: 0, volume: 1 };
     const connection = await message.member.voiceChannel.join();
-    const pattern = new RegExp('(https?://)*(www.)?youtube.com/(.*)');
-    const pattern2 = new RegExp('(https*://)*(www.){0,1}youtu.be/(.*)');
-    let song;
-    let stream;
-    if (pattern.test(args) || pattern2.test(args)) {
-      stream = ytdl(args, { filter : 'audioonly' });
-      song = args;
+    let video;
+
+    if (this.long_url_pattern.test(args) || this.short_url_patter.test(args)) {
+      video = await this.youtube.getVideoByUrl(args);
     }
     else{
-      await YTSearch({ key: process.env.GOOGLE_KEY, term: args }, (videos) => {
-        stream = ytdl('https://www.youtube.com/watch?v=' + videos[0].id.videoId,
-        { filter : 'audioonly' });
-        song = videos[0].snippet.title;
-      });
+      const videos = await this.youtube.search('video', args, 1);
+      video = videos[0];
     }
-    //this.queue.push({stream, song});
-    //if(this.queue.length > 1) {
-      message.channel.sendMessage('Playing: ' + song);
-      connection.playStream(stream, streamOptions);
-    //}
+
+    // this.queue.push({stream, song});
+    // if(this.queue.length > 1) {
+    const song = video.title;
+    const stream = ytdl(video.url, { filter : 'audioonly' });
+    message.channel.send('Playing: ' + song);
+    connection.playStream(stream, streamOptions);
+    // }
   }
 }
 
